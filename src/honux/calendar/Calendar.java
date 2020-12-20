5768917f385c4544a2d397898a51f92b4f313e8c
최윤희 <@LAPTOP-KBJNJSMU>
2020-12-20 16:13:13 +0900

    종료 조건을 이용해 반복

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -21,20 +21,27 @@ public class Calendar {
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수 출력하는 프로그램
+		String PROMPT = "cal> ";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		System.out.print("반복 횟수를 입력하세요>> ");
-		int repeat = scan.nextInt();
+		int month = 1;
 
-		for (int i = 0; i < repeat; i++) {
-			System.out.print("달을 입력하세요>> ");
-			int month = scan.nextInt();
-			
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			month = scan.nextInt();
+			if (month == -1) {
+				break;
+			}
+
+			if (month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
-		
-		System.out.print("== 프로그램을 종료합니다. ==");
+
+		System.out.print("== 프로그램을 종료합니다 ==");
 
 		scan.close();
 	}
